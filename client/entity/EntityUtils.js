ee00ae2f7b30899fd81f99c6729d12d4ffc92453
cam72cam <cam72cam@gmail.com>
2013-03-17 12:19:40 -0400

    Converted to standard entity style

--- client/entity/EntityUtils.js
+++ client/entity/EntityUtils.js
@@ -1,15 +1,7 @@
 namespace("Entity.Utils", function() {
-	// oarg = { bgcolor: '#FFFA', border_color: '#FFF', border_width: X, image: url, opacity: 'XX%'}
 	Entity.Utils.applyTexture = function(div, arg) {
-		if(Utils.IsDefined(arg.bgcolor))
-			$(div).css("background-color", arg.bgcolor);
-		if(Utils.IsDefined(arg.border_color))
-			$(div).css("border-color", arg.border_color);
-		if(Utils.IsDefined(arg.border_width))
-			$(div).css("border-width", arg.border_width);
-		if(Utils.IsDefined(arg.image))
-			$(div).css("background-image", arg.image);
-		if(Utils.IsDefined(arg.opacity))
-			$(div).css("opacity", arg.opacity);
+		for(var key in arg) {
+			$(div).css(key, arg[key]);
+		}
 	}
 });
--- client/models/plane.js
+++ client/models/plane.js
@@ -1,3 +1,3 @@
 plane = [
-	[[0,0,0], [0,0,0], 500, 500, {bgcolor:"#0ff"}],
+	[[0,0,0], [0,0,0], 1000, 1000, { 'background-color' :"#044", 'border': '2px solid #333', 'margin': '0px'}],
 ];
