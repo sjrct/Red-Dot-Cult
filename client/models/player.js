6326fd1a2989dc7ba35c93dfe22a07228346c805
cam72cam <cam72cam@gmail.com>
2013-04-24 08:32:40 -0400

    Updated the player model and animations

--- client/models/player.js
+++ client/models/player.js
@@ -1,6 +1,9 @@
 var foo = new Vector3(80,-100,0);
 var bar = new Vector3(-80,100,0);
 
+var bash = new Vector3(100,-100,0);
+var baz =  new Vector3(-100,100,0);
+
 var fl = new Vector3(100,-300,0);
 var bl = new Vector3(-100,300,0);
 
@@ -10,6 +13,10 @@ player = {
 			'0%'	: { transform: translate3d(foo) + rotate3d(new Vector3(0,0,0)) + translate3d(bar) },
 			'50%'	: { transform: translate3d(foo) + rotate3d(new Vector3(0,-90,0)) + translate3d(bar) },
 			'100%'	: { transform: translate3d(foo) + rotate3d(new Vector3(0,0,0)) + translate3d(bar) },
+		},
+		arm_hack: {
+			'0%'	: { transform: translate3d(foo) + rotate3d(new Vector3(0,-90,0)) + translate3d(bar) },
+			'100%'	: { transform: translate3d(foo) + rotate3d(new Vector3(0,-90,0)) + translate3d(bar) },
 		},
 		lleg: {
 			'0%'	: { transform: translate3d(fl) + rotate3d(new Vector3(45,0,0)) + translate3d(bl) },
@@ -27,10 +34,11 @@ player = {
 
 	objects: [
 		{ name: 'torso', model:'player_torso' },
-		{ name: 'arm_left', model:'player_arm', },
-		{ name: 'arm_right', model:'player_arm', animation:'arm', rotation: new Vector3(0,0,-90) },
+		{ name: 'arm_left', model:'player_arm', animation: 'arm_hack', rotation: new Vector3(0,0,-90), location: new Vector3(180,0,-40)},
+		{ name: 'arm_right', model:'player_arm', animation:'arm', rotation: new Vector3(0,0,-90), },
 		{ name: 'leg_left', model:'player_leg', animation: 'lleg'},
 		{ name: 'leg_right', model:'player_leg', scale: new Vector3(-1,1,1), animation: 'rleg' },
+		{ name: 'gun', model:'gun', location: new Vector3(180,-240,560), rotation: new Vector3(0,90,0), scale: new Vector3(0.5,0.5,0.5) }
 	],
 	
 
